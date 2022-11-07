b930bb938b16971d056e94dac262f7e1e5b5733e
Ayush Singh <74140962+ayushsingh-07@users.noreply.github.com>
2022-11-07 21:09:50 +0530

    A basic HTML template is added and view funtion is changed to render that template

--- playground/views.py
+++ playground/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def hello_word(request):
-    return HttpResponse('Hello Bhai Log!')
+    return render(request,'hello.html',{"Name":"Ayush"})
